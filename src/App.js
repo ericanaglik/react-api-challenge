0f58224c381b4c47e25911609df16db39c721f5d
erica2605@live.com <erica2605@live.com>
2019-04-09 19:40:44 -0700

    this isnt working but i'm trying my best

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import React, { Component } from 'react';
 
+import Weather from './Weather'
+
 import './App.css';
 
 /** 
@@ -58,35 +60,6 @@ class App extends Component {
     })
   }
 
-  renderWeather() {
-    // This method returns undefined or a JSX component
-    if (this.state.weatherData === null) {
-      // If there is no data return undefined
-      return undefined
-    }
-
-    /* 
-    This next step needs another level of error checking. It's 
-    possible to get a JSON response for an invalid zip in which 
-    case the step below fails. 
-    */ 
-    console.log(this.state.weatherData)
-    // Take the weather data apart to more easily populate the component
-    const { main, description, icon } = this.state.weatherData.weather[0]
-    const { temp, pressure, humidity, temp_min, temp_max } = this.state.weatherData.main 
-    
-    return (
-      <div>
-        <div>Title: {main}</div>
-        <div>Desc: {description}</div>
-        <div>Icon: {icon}</div>
-        <div>Temp: {temp}</div>
-        <div>Pressure: {pressure}</div>
-        <div>Humidity: {humidity}</div>
-        <div>Temp Min: {temp_min} Max:{temp_max}</div>
-      </div>
-    )
-  }
 
   render() {
     return (
@@ -113,7 +86,7 @@ class App extends Component {
         </form>
 
         {/** Conditionally render this component */}
-        {this.renderWeather()}
+        { Weather }
 
       </div>
     );
--- src/Weather.js
+++ src/Weather.js
@@ -1,1 +1,32 @@
 import React from 'react'
+
+function Weather(props) {
+    // This method returns undefined or a JSX component
+    if (this.state.weatherData === null) {
+        // If there is no data return undefined
+        return undefined
+    }
+    /* 
+    This next step needs another level of error checking. It's 
+    possible to get a JSON response for an invalid zip in which 
+    case the step below fails. 
+    */ 
+    console.log(this.state.weatherData)
+    // Take the weather data apart to more easily populate the component
+    const { main, description, icon } = this.state.weatherData.weather[0]
+    const { temp, pressure, humidity, temp_min, temp_max } = this.state.weatherData.main 
+    
+    return (
+        <Weather
+        Title={main}
+        Desc={description}
+        Icon={icon}
+        Temp={temp}
+        Pressure={pressure}
+        Humidity={humidity}
+        Min={temp_min} 
+        Max={temp_max}
+    />)
+}
+
+export default Weather
